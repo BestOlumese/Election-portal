52746a96e04c47a733ba8d3d7bd059fa9db6e705
Amiolemen Best <bestolumese@gmail.com>
2026-01-18 22:13:16 +0100

    Election app test

--- config/db.js
+++ config/db.js
@@ -7,7 +7,11 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306, // Added port here
+    ssl: {
+        rejectUnauthorized: true // TiDB/Cloud DBs often require SSL
+    }
 });
 
 module.exports = pool.promise();
